0ebc4abab3185281a587e2f85b3fe42df7717816
Vander Sandino <65673565+vandersann@users.noreply.github.com>
2023-12-28 19:03:48 -0300

    rebranding

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".slide-characters", {
+    slidesPerView: 3.5,
+    spaceBetween: 19,
+    freeMode: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1.1,
+            spaceBetween: 19,
+        },
+        768: {
+            slidesPerView: 2.2,
+            spaceBetween: 19,
+        },
+        991: {
+            slidesPerView: 2.8,
+            spaceBetween: 19,
+        },
+        1200: {
+            slidesPerView: 3.5,
+            spaceBetween: 19,
+        },
+    },
+});
+
+AOS.init({
+    duration: 1000,
+    once: true,
+});
